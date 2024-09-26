830f7e2f8093ff74787b1a165792f254cf402f98
Erik Riquelme <erikone2311@gmail.com>
2024-09-26 11:55:57 +0200

    Add Feature 3: Delete student

--- src/students/service/studentsService.ts
+++ src/students/service/studentsService.ts
@@ -12,11 +12,42 @@ export const getStudentsTotal = (): number => {
 // Crea una función para añadir un estudiante a la lista de estudiantes
 // La función debe recibir un array de estudiantes y los datos del estudiante a añadir
 // Si el estudiante ya existe en la lista, muestra un error con showErrorModal
-// export const addStudent =
+export const addStudent = (
+  students: Student[],
+  studentName: Student["name"],
+  studentSurname: Student["lastName"],
+  studentAge: Student["age"],
+  studentEmail: Student["email"],
+  studentPhone: Student["phoneNumber"]
+): void => {
+  let newStudent: Student = {
+    id: generateId(students),
+    name: studentName,
+    lastName: studentSurname,
+    age: studentAge,
+    email: studentEmail,
+    phoneNumber: studentPhone,
+  };
+  for (let position = 0; position < students.length; position++) {
+    if (students[position].email === newStudent.email) {
+      showErrorModal("El estudiante ya esta en la lista");
+      break;
+    } else {
+      students.push(newStudent);
+      break;
+    }
+  }
+};
 
 // Crea una función para eliminar un estudiante de la lista de estudiantes
 // La función debe recibir un array de estudiantes y el id del estudiante a eliminar
-// export const deleteStudent =
+export const deleteStudent = (students: Student[], studentId: number): void => {
+  for (let position = 0; position < students.length; position++) {
+    if (students[position].id === studentId) {
+      students.splice(position, 1);
+    }
+  }
+};
 
 // Crea una función para obtener las opciones de estudiantes para rellenar un select
 // La función debe recibir un array de estudiantes
